c0d7db33c328b70d3de58a2a1be00e7f6f6ee7fd
Xenos Konstantinos <mr.xkon@gmail.com>
2018-08-30 09:30:19 +0300

    add installation size
    add installation size

--- classes/class-wp-live-debug-wordpress-info.php
+++ classes/class-wp-live-debug-wordpress-info.php
@@ -34,23 +34,138 @@ if ( ! class_exists( 'WP_Live_Debug_WordPress_Info' ) ) {
 		}
 
 		public static function create_page() {
-			?>
-				<div class="sui-box">
-					<div class="sui-box-header">
-						<h2 class="sui-box-title">Constants</h2>
-					</div>
-					<div class="sui-box-body">
-						<table class="sui-table striped">
-							<tbody>
-								<?php WP_Live_Debug::table_info( WP_Live_Debug_WordPress_Info::get_wp_info() ); ?>
-							</tbody>
-						</table>
-					</div>
+		?>
+			<div class="sui-box">
+				<div class="sui-box-header">
+					<h2 class="sui-box-title">Installation Size</h2>
 				</div>
-			<?php
+				<div class="sui-box-body">
+					<table class="sui-table striped">
+						<tbody>
+							<?php WP_Live_Debug_WordPress_Info::get_installation_size(); ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="sui-box">
+				<div class="sui-box-header">
+					<h2 class="sui-box-title">Constants</h2>
+				</div>
+				<div class="sui-box-body">
+					<table class="sui-table striped">
+						<tbody>
+							<?php WP_Live_Debug::table_info( WP_Live_Debug_WordPress_Info::get_wp_constants() ); ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		<?php
 		}
 
-		public static function get_wp_info() {
+		public static function get_installation_size() {
+			$uploads_dir = wp_upload_dir();
+
+			$sizes = array(
+				'wp'      => array(
+					'path' => ABSPATH,
+					'size' => 0,
+				),
+				'themes'  => array(
+					'path' => trailingslashit( get_theme_root() ),
+					'size' => 0,
+				),
+				'plugins' => array(
+					'path' => WP_PLUGIN_DIR,
+					'size' => 0,
+				),
+				'uploads' => array(
+					'path' => $uploads_dir['basedir'],
+					'size' => 0,
+				),
+			);
+
+			$inaccurate = false;
+
+			foreach ( $sizes as $size => $attributes ) {
+				try {
+					$sizes[ $size ]['size'] = WP_Live_Debug_WordPress_Info::get_directory_size( $attributes['path'] );
+				} catch ( Exception $e ) {
+					$inaccurate = true;
+				}
+			}
+
+			$size_db = WP_Live_Debug_WordPress_Info::get_database_size();
+
+			$size_total = $sizes['wp']['size'] + $size_db;
+
+			$arrays = array(
+				array(
+					'label' => __( 'Uploads Directory', 'health-check' ),
+					'value' => size_format( $sizes['uploads']['size'], 2 ),
+				),
+				array(
+					'label' => __( 'Themes Directory', 'health-check' ),
+					'value' => size_format( $sizes['themes']['size'], 2 ),
+				),
+				array(
+					'label' => __( 'Plugins Directory', 'health-check' ),
+					'value' => size_format( $sizes['plugins']['size'], 2 ),
+				),
+				array(
+					'label' => __( 'Database size', 'health-check' ),
+					'value' => size_format( $size_db, 2 ),
+				),
+				array(
+					'label' => __( 'Whole WordPress Directory', 'health-check' ),
+					'value' => size_format( $sizes['wp']['size'], 2 ),
+				),
+				array(
+					'label' => __( 'Total installation size', 'health-check' ),
+					'value' => sprintf(
+						'%s %s',
+						size_format( $size_total, 2 ),
+						( false === $inaccurate ? '' : __( 'Invalid permissions found, some values  may be inaccurate.', 'wp-live-debug' ) )
+					),
+				),
+			);
+
+			$result = '';
+
+			foreach ( $arrays as $array ) {
+				$result .= '<tr>';
+				$result .= '<td>' . $array['label'] . '</td>';
+				$result .= '<td>' . $array['value'] . '</td>';
+				$result .= '</tr>';
+			}
+
+			echo $result;
+		}
+
+		public static function get_directory_size( $path ) {
+			$size = 0;
+
+			foreach ( new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $path ) ) as $file ) {
+				$size += $file->getSize();
+			}
+
+			return $size;
+		}
+
+		public static function get_database_size() {
+			global $wpdb;
+			$size = 0;
+			$rows = $wpdb->get_results( 'SHOW TABLE STATUS', ARRAY_A );
+
+			if ( $wpdb->num_rows > 0 ) {
+				foreach ( $rows as $row ) {
+					$size += $row['Data_length'] + $row['Index_length'];
+				}
+			}
+
+			return $size;
+		}
+
+		public static function get_wp_constants() {
 			global $wp_version;
 
 			$wp        = array();
