cae2db8bc5490331ebb97cbec233191e37a68b87
Konstantinos Xenos <mr.xkon@gmail.com>
2019-12-16 19:14:02 +0200

    Update class-wp-live-debug.php

--- class-wp-live-debug.php
+++ class-wp-live-debug.php
@@ -1,46 +1,35 @@
-<?php //phpcs:ignore
+<?php //phpcs:ignore -- \r\n notice.
+
 /**
  *
  * Plugin Name:       WP Live Debug
  * Description:       Enables debugging and adds a screen into the WordPress Admin to view the debug.log.
- * Version:           4.9.8.6
- * Author:            Konstantinos (xkon) Xenos
+ * Version:           5.3.1
+ * Author:            Konstantinos Xenos
  * Author URI:        https://xkon.gr
- * License:           GPL-2.0+
+ * License:           GPLv2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-live-debug
  * Domain Path:       /languages
  *
  */
 
-/*
-Copyright Konstantinos Xenos ( https://xkon.gr )
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
-/*
-Credits & Licences:
-This is a personal project that I use for debugging, but some parts of the code are written by other awesome people.
-
-So props also go to:
-WPMU DEV ( https://premium.wpmudev.org ) for parts of debug info & Shared UI
-- Shared UI ( https://github.com/wpmudev/shared-ui ) - Licence GPLv2
-
-The WordPress.org ( https://wordpress.org ) community for parts of debug info
-- Health Check ( https://wordpress.org/plugins/health-check/ ) - Licence GPLv2 - ( https://github.com/wordpress/health-check )
-*/
+/**
+ * Copyright (C) 2019 Konstantinos Xenos (https://xkon.gr).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
 
 /**************************************************/
 /****************** Plugin Start ******************/
@@ -52,12 +41,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Define the plugin version for internal use
+ * Various constants.
  */
-define( 'WP_LIVE_DEBUG_VERSION', '4.9.8.6' );
+define( 'WP_LIVE_DEBUG_VERSION', '5.3.1' );
 define( 'WP_LIVE_DEBUG_WP_CONFIG', ABSPATH . 'wp-config.php' );
 define( 'WP_LIVE_DEBUG_WP_CONFIG_BACKUP_ORIGINAL', ABSPATH . 'wp-config.wpld-original-backup.php' );
 define( 'WP_LIVE_DEBUG_WP_CONFIG_BACKUP', ABSPATH . 'wp-config.wpld-manual-backup.php' );
+
 /**
  * WP_Live_Debug Class.
  */
@@ -194,18 +184,6 @@ if ( ! class_exists( 'WP_Live_Debug' ) ) {
 		public static function enqueue_scripts_styles( $hook ) {
 			if ( 'toplevel_page_wp-live-debug' === $hook ) {
 				wp_enqueue_style(
-					'wphb-wpmudev-sui',
-					plugin_dir_url( __FILE__ ) . 'assets/sui/css/shared-ui.min.css',
-					'2.2.10'
-				);
-				wp_enqueue_script(
-					'wphb-wpmudev-sui',
-					plugin_dir_url( __FILE__ ) . 'assets/sui/js/shared-ui.min.js',
-					array( 'jquery' ),
-					'2.2.10',
-					true
-				);
-				wp_enqueue_style(
 					'wp-live-debug',
 					plugin_dir_url( __FILE__ ) . 'assets/styles.css',
 					array( 'wphb-wpmudev-sui' ),
@@ -229,11 +207,11 @@ if ( ! class_exists( 'WP_Live_Debug' ) ) {
 		 *
 		 * @return string $classes Updated classes list including the Shared-UI classes.
 		 */
-		public static function admin_body_classes( $classes ) {
-			$classes .= ' sui-2-2-10 ';
+		// public static function admin_body_classes( $classes ) {
+		// 	$classes .= ' sui-2-2-10 ';
 
-			return $classes;
-		}
+		// 	return $classes;
+		// }
 
 		/**
 		 * Create the WP Live Debug page.
@@ -367,20 +345,20 @@ if ( ! class_exists( 'WP_Live_Debug' ) ) {
 
 	// Require extra files
 	require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-live-debug.php';
-	require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-wordpress-info.php';
-	require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-server-info.php';
-	require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-cronjob-info.php';
-	require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-tools.php';
-	require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-wpmudev.php';
+	// require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-wordpress-info.php';
+	// require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-server-info.php';
+	// require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-cronjob-info.php';
+	// require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-tools.php';
+	// require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-wpmudev.php';
 	require_once plugin_dir_path( __FILE__ ) . '/classes/class-wp-live-debug-helper.php';
 
 	// Initialize Classes.
 	new WP_Live_Debug();
 	new WP_Live_Debug_Live_Debug();
-	new WP_Live_Debug_WordPress_Info();
-	new WP_Live_Debug_Server_Info();
-	new WP_Live_Debug_Cronjob_Info();
-	new WP_Live_Debug_Tools();
-	new WP_Live_Debug_WPMUDEV();
+	// new WP_Live_Debug_WordPress_Info();
+	// new WP_Live_Debug_Server_Info();
+	// new WP_Live_Debug_Cronjob_Info();
+	// new WP_Live_Debug_Tools();
+	// new WP_Live_Debug_WPMUDEV();
 	new WP_Live_Debug_Helper();
 }
